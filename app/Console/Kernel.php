74229d06e6fc1ef774ddcf88b56c8e8ec5e66fc6
simmonsec <sistemas@simmons.com.ec>
2024-07-29 12:16:31 -0500

    TAREA PROGRAMADA

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,12 +7,22 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    /** 
+     * The Artisan commands provided by your application. 
+     * 
+     * @var array 
+     */
+    protected $commands = [ 
+        'App\Console\Commands\Comunicaciones\InventarioTerceros'
+    ];
+
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('inventario:terceros')->everyMinute(); //everyMinute//everyFifteenMinutes,everyTwentySeconds
     }
 
     /**
