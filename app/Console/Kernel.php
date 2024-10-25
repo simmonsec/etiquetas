75d5043328946f1d0419d5bc630bc7ca19c57350
simmonsec <sistemas@simmons.com.ec>
2024-10-25 16:13:08 -0500

    cambios en la tarea

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -45,7 +45,7 @@ class Kernel extends ConsoleKernel
          * Aplicación de Producción Eventos
          */
         $schedule->command('syncAppSheetPostgres:produccionEventos')
-            ->everyTwoMinutes()
+            ->everyMinute()
             ->onOneServer()
             ->runInBackground()
             ->withoutOverlapping();
@@ -54,7 +54,7 @@ class Kernel extends ConsoleKernel
          * Aplicación de Visitas de Exhibiciones
          */
         $schedule->command('syncAppSheetPostgres:exhibicionVisita')
-            ->everyMinute()
+            ->everyTwoMinutes()
             ->onOneServer()
             ->runInBackground()
             ->withoutOverlapping();
