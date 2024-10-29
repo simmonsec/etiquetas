60fab131d7230a2490d1168307376799b4328ead
simmonsec <sistemas@simmons.com.ec>
2024-10-29 09:58:20 -0500

    pruebas de migraciones

--- app/Console/Commands/Migraciones/GnlMigrarDatosOdbc.php
+++ app/Console/Commands/Migraciones/GnlMigrarDatosOdbc.php
@@ -34,9 +34,10 @@ class GnlMigrarDatosOdbc extends Command
                 print_r("\n");
                 Log::info("Entro a eliminar datos: {$parametro->d_comando}");
                 DB::statement($parametro->d_comando);
+
             }
 
-      
+
             // Datos para la conexión 4D
             $q_dsn = $parametro->q_dsn;
             $q_user = $parametro->q_user;
@@ -53,14 +54,14 @@ class GnlMigrarDatosOdbc extends Command
             // Crear instancias de conexión para 4D y PostgreSQL
             $db4DService = new Conexion4k($q_dsn, $q_user, $q_password);
             $dbPostgresService = new ConexionPostgres($i_dsn, $i_user, $i_password);
-            
+
             // Obtener la conexión desde el servicio
             $connection4D = $db4DService->getConnection();
 
             $connectionPostgres = $dbPostgresService->getConnection();
 
             // Procesar datos en lotes con paginación
-            $batchSize = 100;
+            $batchSize = 1;
             $offset = 0;
             $resultado = [];
             // Ejecuta la consulta para obtener la cantidad
@@ -68,13 +69,13 @@ class GnlMigrarDatosOdbc extends Command
                 // Remplazar la lista de columnas por COUNT(*)
                 $q_comando_count = preg_replace('/SELECT\s+.*\s+FROM/s', 'SELECT COUNT(*) FROM', $q_comando);
                 // Eliminar ORDER BY si existe
-              
+
                 $q_comando_count = preg_replace('/ORDER BY .*/', '', $q_comando_count);
                 $resultado = $this->consulta($connection4D, $q_comando_count);
-               
+
             }
             log::info($resultado);
-         
+
             if (!empty($resultado)) {
                 $cantidad = $resultado[0]["<expression>"] ?? count($resultado);
                 print_r("Cantidad de registros: " . $cantidad . "\n");
@@ -91,18 +92,30 @@ class GnlMigrarDatosOdbc extends Command
 
                     $q_comandoPaginado = $q_comando . " LIMIT $batchSize OFFSET $offset";
                     $datos = $this->consulta($connection4D, $q_comandoPaginado);
-                     
-                 
+
+
                     if (!empty($datos)) {
                         // Insertar datos por lotes en PostgreSQL
-                    
+                       
+
                         try {
+                            // Convertir los datos a UTF-8 si no están en esa codificación
+                            foreach ($datos as &$dato) {
+                                foreach ($dato as $key => $value) {
+                                    // Convertir solo si el valor es una cadena
+                                    if (is_string($value)) {
+                                        $dato[$key] = mb_convert_encoding($value, 'UTF-8', 'auto');
+                                    }
+                                }
+                            }
+                            log::info($datos);
+
                             $success = $this->insertarBatch($connectionPostgres, $i_comando, $datos);
-                            
+
                         } catch (\Exception $th) {
                             Log::error("ERROR INSERT: " . $th);
                         }
- 
+
 
                         $offset += $batchSize;
                         $restante = $cantidad - $offset;
@@ -151,13 +164,14 @@ class GnlMigrarDatosOdbc extends Command
         $retries = 0;
         $success = false;
         $results = [];
-
+        
         while ($retries < $maxRetries && !$success) {
             try {
-                //Log::info('Conexión existente a la base de datos ODBC.');
+                Log::info($connection);
+                Log::info($sql);
 
                 $result = odbc_exec($connection, $sql);
-             
+
                 if (!$result) {
                     throw new \Exception("Error al ejecutar la consulta: " . odbc_errormsg($connection));
                 } else {
@@ -207,57 +221,58 @@ class GnlMigrarDatosOdbc extends Command
         $maxRetries = 3;
         $retries = 0;
         $success = false;
-    
+
         if ($connection) {
             $values = [];
-    
+
             foreach ($batch as $fila) {
-                
                 // Escapar y formatear los valores
                 $escapedValues = array_map(function ($value) {
-                    // Verificar si el valor es NULL
-                    
                     if (is_null($value)) {
                         return 'NULL';
                     }
-                   
+                    // Si es un valor de tipo string, encerrarlo entre comillas simples
+                    elseif (is_string($value)) {
+                        return "'" . str_replace("'", "''", $value) . "'";
+                    }
                     // Devolver el valor tal como está para otros tipos
                     else {
                         return $value;
                     }
                 }, array_values($fila));
-    
+
                 // Crear la fila de valores para insertar
                 $values[] = '(' . implode(', ', $escapedValues) . ')';
             }
-    
+
             // Crear la consulta completa con los valores
             $query = $sql . ' ' . implode(', ', $values);
-
+            Log::info($query);
             while ($retries < $maxRetries && !$success) {
-            
+                Log::info($query);
+
                 try {
                     // Ejecutar la consulta
                     $result = odbc_exec($connection, $query);
-    
+
                     // Verificar si la consulta fue exitosa
                     if (!$result) {
                         throw new \Exception(odbc_errormsg($connection));
                     }
-    
+
                     // Si es exitosa, marcar como éxito
                     $success = true;
                     return true;
                 } catch (\Exception $e) {
                     $retries++;
                     Log::warning("Error al ejecutar el INSERT: " . $e->getMessage() . " - Reintento {$retries} de {$maxRetries}");
-    
+
                     // Si se alcanza el número máximo de intentos
                     if ($retries >= $maxRetries) {
                         Log::error("Error después de {$retries} intentos: " . $e->getMessage());
                         return false;
                     }
-    
+
                     // Esperar 1 segundo antes de reintentar
                     sleep(1);
                 }
@@ -267,7 +282,8 @@ class GnlMigrarDatosOdbc extends Command
             return false;
         }
     }
-    
+
+
     /**
      * Función para verificar si un valor es una fecha válida
      */
@@ -281,7 +297,7 @@ class GnlMigrarDatosOdbc extends Command
             return false; // Si no se puede crear, la fecha no es válida
         }
     }
-    
+
 
 
 
